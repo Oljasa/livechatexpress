a1612ef820a67142afded27d73851b671c752f30
oljasa <oljasalishev@gmail.com>
2025-02-22 18:27:03 -0600

    working initial version, joining example-room from frontend

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,66 @@
+// import logger from 'jet-logger';
+
+// import ENV from '@src/common/ENV';
+// import server from './server';
+
+
+// /******************************************************************************
+//                                   Run
+// ******************************************************************************/
+
+// const SERVER_START_MSG = ('Express server started on port: ' + 
+//   ENV.Port.toString());
+
+// server.listen(ENV.Port, () => logger.info(SERVER_START_MSG));
+import express from 'express';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
 import logger from 'jet-logger';
 
 import ENV from '@src/common/ENV';
-import server from './server';
+import app from './server';
 
+// Create HTTP server
+const server = http.createServer(app);
 
-/******************************************************************************
-                                  Run
-******************************************************************************/
+// Configure CORS
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+};
 
-const SERVER_START_MSG = ('Express server started on port: ' + 
-  ENV.Port.toString());
+// Initialize Socket.IO
+const io = new SocketIOServer(server, {
+  cors: corsOptions,
+});
 
-server.listen(ENV.Port, () => logger.info(SERVER_START_MSG));
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+  logger.info('A user connected');
+
+  // Handle 'joinRoom' event
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    logger.info(`User joined room: ${room}`);
+  });
+
+  // Handle 'chatMessage' event
+  socket.on('chatMessage', (msg) => {
+    const { room, message } = msg;
+    io.to(room).emit('chatMessage', {id: socket.id, message});
+    logger.info(`Message sent to room ${room}: ${message}`);
+  });
+
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    logger.info('A user disconnected');
+  });
+});
+
+// Start the server
+const PORT = ENV.Port || 3000;
+server.listen(PORT, () => {
+  logger.info(`Server started on port: ${PORT}`);
+});

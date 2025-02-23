c70632bbf160473487f234b899a8bdb568ded2b1
oljasa <oljasalishev@gmail.com>
2025-02-22 22:43:56 -0600

    move server call into index.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@ import ENV from '@src/common/ENV';
 import HttpStatusCodes from '@src/common/HttpStatusCodes';
 import { RouteError } from '@src/common/route-errors';
 import { NodeEnvs } from '@src/common/constants';
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
 
 
 /******************************************************************************
@@ -80,4 +82,61 @@ app.get('/users', (_: Request, res: Response) => {
                                 Export default
 ******************************************************************************/
 
+// Create HTTP server
+const server = http.createServer(app);
+console.log('here!!')
+
+// Configure CORS
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+};
+console.log('here2')
+
+// Initialize Socket.IO
+const io = new SocketIOServer(server, {
+  cors: corsOptions,
+});
+
+// Socket.IO connection handler
+console.log('here')
+
+console.log('io', io)
+io.on('connection', (socket) => {
+  logger.info('A user connected');
+
+  // Handle 'joinRoom' event
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`Socket ${socket.id} joined room ${room}`);
+  });
+
+  // Handle 'chatMessage' event
+  socket.on('chatMessage', (msg) => {
+    console.log("msg:   " + msg.room)
+    const { room, message } = msg;
+    io.to(room).emit('chatMessage', {id: socket.id, message, room: msg.room});
+    logger.info(`Message sent to room ${room}: ${message}`);
+  });
+
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    logger.info('A user disconnected');
+  });
+  
+  socket.on('leaveRoom', (room) => {
+    socket.leave(room);
+    console.log(`Socket ${socket.id} left room ${room}`);
+    // Optionally, notify others.
+    socket.to(room).emit('systemMessage', `User ${socket.id} left the room.`);
+  });
+});
+
+// Start the server
+const PORT = ENV.Port || 3000;
+server.listen(PORT, () => {
+  logger.info(`Server started on port: ${PORT}`);
+});
 export default app;
